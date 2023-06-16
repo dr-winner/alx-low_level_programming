00f368e1ea1dc3fdb21864fad8b58ad8675d6c33
dr-winner <duvorrichardwinner@gmail.com>
2023-06-15 23:02:55 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - a program that prints the size of various types on the computer it is compiled and run on
+ * main - a program that prints the size of various types on the computer
  * Return 0 Success
  */
-int main (void)
+int main(void)
 {
 	char a;
 	int b;
