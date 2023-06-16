9585b0f14918cc1b40b4d83544bc938b5174c1b4
dr-winner <duvorrichardwinner@gmail.com>
2023-06-15 22:38:09 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this program prints using the function printf
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+return (0);
+}
