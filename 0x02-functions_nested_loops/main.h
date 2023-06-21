1587df8c5a33a7275449c8c759ca9d23dc876a79
dr-winner <duvorrichardwinner@gmail.com>
2023-06-20 18:34:01 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char);
 void print_alphabet(void);
 int _abs(int);
 void print_alphabet_x10(void);
@@ -12,6 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n)
+void print_times_table(int n);
 
 #endif
