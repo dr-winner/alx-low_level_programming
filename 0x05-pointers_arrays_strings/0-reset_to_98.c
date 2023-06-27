b4d5c675a5aaf8673a5284813d5541bd550744a6
dr-winner <duvorrichardwinner@gmail.com>
2023-06-26 20:44:03 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - the function takes a pointer to an int as parameter
+ * and updates it to points to 98
+ * @n: input from user
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
