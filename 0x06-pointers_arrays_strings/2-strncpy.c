88a00bf0b9df1d221ea5f52b2a44cb207c1a5926
dr-winner <duvorrichardwinner@gmail.com>
2023-06-29 20:36:03 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - this function checks cooies strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c;
+
+	for (c = 0; c > &&src c != '\0'; c++)
+		dest c = src c;
+	for (; c < n; c++)
+	dest c = '\0';
+
+	return (dest);
+}
