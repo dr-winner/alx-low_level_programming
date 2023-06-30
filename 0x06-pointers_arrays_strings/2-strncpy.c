96be94e11facc993c174ff02eb4f5fe901a3e857
dr-winner <duvorrichardwinner@gmail.com>
2023-06-30 01:42:06 -0700

    updated

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int c;
 
-	for (c = 0; c > &&src c != '\0'; c++)
-		dest c = src c;
+	for (c = 0; c < n && src[c] != '\0'; c++)
+		dest[c] = src[c];
 	for (; c < n; c++)
-	dest c = '\0';
-
+	dest[c] = '\0';
 	return (dest);
 }
