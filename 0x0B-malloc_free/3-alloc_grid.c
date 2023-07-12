e9937943a6eb8614bf3cdb8d644ec1733b83d4da
dr-winner <duvorrichardwinner@gmail.com>
2023-07-12 04:16:32 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,23 +17,24 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	m = malloc(sizeof(int *) * height);
-	if (m == NULL)
-		return (NULL);
+
+		if (m == NULL)
+			return (NULL);
 		for (n = 0; n < height; n++)
 		{
 			m[n] = malloc(sizeof(int) * width);
-				if (m[n] == NULL)
-				{
-					for (; n >= 0; n--)
-						free(m[n]);
-					free(m);
-					return (NULL);
-				}
+			if (m[n] == NULL)
+			{
+				for (; n >= 0; n--)
+					free(m[n]);
+				free(m);
+				return (NULL);
+			}
 		}
 		for (n = 0; n < height; n++)
 		{
 			for (i = 0; i < width; i++)
 				m[n][i] = 0;
 		}
-	return (m);
+		return (m);
 }
