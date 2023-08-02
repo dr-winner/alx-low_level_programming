5e0316b324c84f85eba9ca2b3583710ff77925ea
dr-winner <duvorrichardwinner@gmail.com>
2023-08-01 18:03:55 -0700

    corrected tasks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t myNum = 0;
+	size_t myNum = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- myNum++;
- h = h->next;
- }
-
- return (myNum);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		myNum++;
+		h = h->next;
+	}
+	
+	return (myNum);
 }
