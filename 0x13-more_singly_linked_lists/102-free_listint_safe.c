89f2c7329bcc31b2df42b82f1ebd74002b97913d
dr-winner <duvorrichardwinner@gmail.com>
2023-08-02 21:48:47 -0700

    task 102 corrected

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -7,33 +7,32 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
- size_t length = 0;
- int diff;
- listint_t *temp;
+	size_t length = 0;
 
- if (!h || !*h)
- return (0);
+	int diff;
 
- while (*h)
- {
- diff = *h - (*h)->next;
- if (diff > 0)
- {
- temp = (*h)->next;
- free(*h);
- *h = temp;
- length++;
- }
- else
- {
- free(*h);
- *h = NULL;
- length++;
- break;
- }
- }
+	listint_t *temp;
 
- *h = NULL;
-
- return (length);
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			length++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			length++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (length);
 }
